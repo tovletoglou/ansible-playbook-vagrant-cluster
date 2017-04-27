052b531240f6a733b59ddcf3d1083cae797c176b
Apostolos Tovletoglou <tovletoglou@gmail.com>
2017-04-27 11:10:08 +0100

    Add --full status to ./git_check_status.sh

--- git_check_status.sh
+++ git_check_status.sh
@@ -1,41 +1,50 @@
-#!/bin/bash
+#!/bin/sh
 
 # This script is used for development purpose only in order to check the `git
 # status` on the main playbook and the sub-projects roles in the `roles/`
 # directory.
-#
-# Ansible Galaxy doesn't give the SCM files (.git) of a role:
-# http://stackoverflow.com/questions/40469084/ansible-galaxy-keep-scm-files
-#
-# So we `git clone` the roles with `ansible-playbook -i hosts playbook_ansible.yml`
-# and ignore them by the main project in the `.gitignore` file.
-#
-# Finally check the project status we run this script `./git_check_status.sh`.
 
-# Find the path of the script.
-[[ -n ${ZSH_VERSION-} ]] &&
-  PROJECT_ROOT="$0" ||
-  PROJECT_ROOT="$BASH_SOURCE"
-[[ $PROJECT_ROOT =~ ^/ ]] ||
-  PROJECT_ROOT="$PWD/$PROJECT_ROOT"
+# Master ansible playbook directory.
+DIRECTORY="$(dirname "$(realpath "$0")")/"
+echo
+while test $# -gt 0; do
+  case "$1" in
+    -f|--full)
+            # Check the playbook status.
+              echo -e "\e[32mrepo: $DIRECTORY\e[0m"
+              git --git-dir="$DIRECTORY".git --work-tree=$DIRECTORY status
+              echo
 
-PROJECT_FILE=$PROJECT_ROOT
-PROJECT_ROOT="$(cd "$(dirname $PROJECT_ROOT)"; pwd)"
+            # Check the status of all sub-projects in the `roles/` directory.
+            for d in "$DIRECTORY"roles/*/; do
+                echo -e "\e[32mrepo: $d\e[0m"
+                git --git-dir=$d.git --work-tree=$d status
+                echo
+            done
+            exit 0
+            ;;
+    *)
+            echo -e "\e[31mWrong parameter. Printing default output:\e[0m"
+            echo
+            break
+            ;;
+  esac
+done
 
-# Check playbook status.
-if [[ $(git -C "$PROJECT_ROOT" status -s) ]]; then
-  echo -e "\e[31mXX Directory:\e[0m $PROJECT_ROOT"
-  git -C "$PROJECT_ROOT" status -s
+# Check the playbook status.
+if [[ $(git --git-dir="$DIRECTORY".git --work-tree=$DIRECTORY status -s) ]]; then
+  echo -e "\e[31mXX repo:\e[0m $DIRECTORY"
+  git --git-dir="$DIRECTORY".git --work-tree=$DIRECTORY status -s
 else
-  echo -e "\e[32mOK Directory:\e[0m $PROJECT_ROOT"
+  echo -e "\e[32mOK repo:\e[0m $DIRECTORY"
 fi
 
 # Check the status of all sub-projects in the `roles/` directory.
-for d in "$PROJECT_ROOT"/roles/*/; do
-  if [[ $(git -C "$d" status -s) ]]; then
-    echo -e "\e[31mXX Directory:\e[0m $d"
-    git -C "$d" status -s
+for d in "$DIRECTORY"roles/*/; do
+  if [[ $(git --git-dir=$d.git --work-tree=$d status -s) ]]; then
+    echo -e "\e[31mXX repo:\e[0m $d"
+    git --git-dir=$d.git --work-tree=$d status -s
   else
-    echo -e "\e[32mOK Directory:\e[0m $d"
+    echo -e "\e[32mOK repo:\e[0m $d"
   fi
 done
